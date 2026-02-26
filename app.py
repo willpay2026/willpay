ac29d31c562e823bf21e61e47ca420925f6d114a
willpay2026 <wildon2024@gmail.com>
2026-02-26 18:52:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -164,4 +164,7 @@ def logout():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render usa la variable de entorno PORT
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
